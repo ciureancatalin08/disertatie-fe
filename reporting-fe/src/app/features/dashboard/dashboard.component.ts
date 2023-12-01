bf157de4b1dbfc468973143241dfc356beabfb6b
cataciurean@rocketmail.com <cataciurean@rocketmail.com>
2023-12-01 16:01:51 +0200

    dashboard fixed

--- reporting-fe/src/app/features/dashboard/dashboard.component.ts
+++ reporting-fe/src/app/features/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 
-import {CookieService} from "ngx-cookie-service";
-import {AuthenticationService} from "../../core/services/authentication/authentication.service";
-import {Router} from "@angular/router";
+import {CookieService} from 'ngx-cookie-service';
+import {AuthenticationService} from '../../core/services/authentication/authentication.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,18 +10,17 @@ import {Router} from "@angular/router";
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  public userName: String;
+  public userName: string;
 
-  constructor(private cookieService: CookieService, private authenticationService: AuthenticationService, private router: Router) {
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
   }
 
   ngOnInit() {
     this.userName = this.authenticationService.getUserName();
   }
 
-
   logout() {
-    localStorage.removeItem("api-token");
+    localStorage.removeItem('api-token');
     this.router.navigate(['login']);
 
   }
